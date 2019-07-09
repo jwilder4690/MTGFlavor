01c50ae542aa5a2da6ea7625df8b01b22cc2a3c2
jwilder4690 <thatoneguy4690@gmail.com>
2019-07-09 06:14:20 -0400

    -Fixed example test methods, though they use deprecated methods in androidx.

--- app/src/androidTest/java/com/justindwilder/tastymtg/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/justindwilder/tastymtg/ExampleInstrumentedTest.java
@@ -1,11 +1,10 @@
 package com.justindwilder.tastymtg;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +20,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.mtgflavorsampler", appContext.getPackageName());
+        assertEquals("com.justindwilder.tastymtg", appContext.getPackageName());
     }
 }
